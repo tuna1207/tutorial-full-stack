cca4df5e73f732ca91efc8c7f77ce6229a8eb5c1
Nguyen Anh Tu <tunguyen.bhtech@gmail.com>
2018-04-13 14:49:50 +0700

    Update home page

--- public/src/App.js
+++ public/src/App.js
@@ -8,11 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Tuna Tech</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Main-container">
+          <img src="https://media.giphy.com/media/mCRJDo24UvJMA/giphy.gif" />
+        </div>
       </div>
     );
   }
